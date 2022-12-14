8dd17b75e5ad4ebcbfcdfc6f8c78c8415b1d0349
martin-centurion <martinalejandrocenturion@gmail.com>
2022-12-14 19:08:49 -0300

    segunda pre entrega

--- js/main.js
+++ js/main.js
@@ -1,268 +1,74 @@
-alert("Primera pre entrega");
-    let nombreUsuario = prompt("Ingrese su nombre");
-    while(nombreUsuario === ""){
-        nombreUsuario = prompt("Ingrese su nombre")
-    }
 
-function solicitarNombre() {
-    alert("Bienvenido " + nombreUsuario + ". Gracias por visitarnos.")
-}
-
-function elegirPrenda() {
-    let prenda;
-    do {
-       prenda = parseInt(prompt("Ingrese la prenda que desea adquirir.\n\nEjemplo seleccione '1' para Remera.\n\n1- Remera\n2- Jeans\n3- Vestido \n4- Saco"));
-    } while (prenda !=1 && prenda !=2 && prenda !=3 && prenda !=4);
-    switch(prenda){
-        case 1:
-            return "Remera";
-        case 2:
-            return "Jeans";
-        case 3:
-            return "Vestido";
-        case 4:
-            return "Saco";
+class Cliente {
+    constructor(nombre, direccion, telefono, mail){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.mail = mail;
     }
 }
 
-function elegirColor() {
-    let color;
-    do {
-        color = parseInt(prompt("Seleccione color.\n\nEjemplo seleccione '1' para Rojo.\n\n1- Rojo \n2- Verde \n3- Amarillo \n4- Azul"));
-    } while (color !=1 && color !=2 && color !=3 && color !=4);
-    switch(color){
-        case 1:
-            return "Rojo";
-        case 2:
-            return "Verde";
-        case 3:
-            return "Amarillo";
-        case 4:
-            return "Azul";
+const clienteUno = new Cliente("Marcos", "Francia 4260, Florida Oeste, Provincia de Buenos Aires", 5491156355415, "marcos@gmail.com");
+const clienteDos = new Cliente("Vanesa", "Leloir 549, Marcos Paz, Provincia de Buenos Aires", 5492525321, "vanesa@gmail.com");
+
+const arrayClientes = [clienteUno, clienteDos];
+
+/* PRODUCTOS */
+
+class Producto {
+    constructor(nombre, precio, id){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.id = id;
     }
 }
 
-function elegirTalle() {
-    let talle;
-    do {
-        talle = parseInt(prompt("Seleccione el talle.\n\nEjemplo seleccione '1' Xs.\n\n1- Xs \n2- S \n3- M \n4- L \n5- Xl"));
-    } while (talle !=1 && talle !=2 && talle !=3 && talle !=4 && talle !=5);
-    switch(talle){
-        case 1:
-            return "Xs";
-        case 2:
-            return "S";
-        case 3:
-            return "M";
-        case 4:
-            return "L";
-        case 5:
-            return "Xl";
+const buzoUno = new Producto("Buzo1", 12400, 1);
+const buzoDos = new Producto("Buzo2", 13000, 2);
+const buzoTres = new Producto("Buzo3", 12000, 3);
+const buzoCuatro = new Producto("Buzo4", 13100, 4);
+
+const articulos = [buzoUno, buzoDos, buzoTres, buzoCuatro];
+
+const arrayProductos = [];
+
+function menu (){
+    alert("Bienvenidos a Tienda de Ropa.");
+    const opcion = parseInt(prompt("Ingrese una opción: \n1) Nuestra tienda.\n2)Finalizar Compra\n3)Salir."));
+    return opcion;
+}
+
+
+function carrito () {
+    let usuarioCompra = parseInt(prompt("Escriba el id del producto que desea adquirir: "));
+    let productoElegido = articulos.find (el => el.id === usuarioCompra);
+    console.log(productoElegido);
+    if(productoElegido === undefined){
+        alert("Ingrese un numero valido por favor");
+        carrito();
     }
+    arrayProductos.push(productoElegido.precio)
 }
 
-function validarPrecio (prenda, color, talle) {
-
-    if (prenda === "Remera" && color === "Rojo" && talle === "Xs") {
-                return 500;
-    } else if (prenda === "Remera" && color === "Rojo" && talle === "S") {
-                return 550;
-    } else if (prenda === "Remera" && color === "Rojo" && talle === "L") {
-                return 600;
-    } else if (prenda === "Remera" && color === "Rojo" && talle === "M") {
-                return 650;     
-    } else if (prenda === "Remera" && color === "Rojo" && talle === "Xl") {
-                return 700;
-    } else if (prenda === "Remera" && color === "Verde" && talle === "Xs") {
-                return 500;
-    } else if (prenda === "Remera" && color === "Verde" && talle === "S") {
-                return 550;
-    } else if (prenda === "Remera" && color === "Verde" && talle === "L") {
-                return 600;
-    } else if (prenda === "Remera" && color === "Verde" && talle === "M") {
-                return 650;     
-    } else if (prenda === "Remera" && color === "Verde" && talle === "Xl") {
-            return 700;
-    } else if (prenda === "Remera" && color === "Amarillo" && talle === "Xs") {
-            return 500;
-    } else if (prenda === "Remera" && color === "Amarillo" && talle === "S") {
-            return 550;
-    } else if (prenda === "Remera" && color === "Amarillo" && talle === "L") {
-            return 600;
-    } else if (prenda === "Remera" && color === "Amarillo" && talle === "M") {
-            return 650;     
-    } else if (prenda === "Remera" && color === "Amarillo" && talle === "Xl") {
-        return 700;
-    } else if (prenda === "Remera" && color === "Azul" && talle === "Xs") {
-        return 500;
-    } else if (prenda === "Remera" && color === "Azul" && talle === "S") {
-        return 550;
-    } else if (prenda === "Remera" && color === "Azul" && talle === "L") {
-        return 600;
-    } else if (prenda === "Remera" && color === "Azul" && talle === "M") {
-        return 650;     
-    } else if (prenda === "Remera" && color === "Azul" && talle === "Xl") {
-    return 700;
-
-    //jeans
-    
-    } if (prenda === "Jeans" && color === "Rojo" && talle === "Xs") {
-        return 1500;
-    } else if (prenda === "Jeans" && color === "Rojo" && talle === "S") {
-        return 1550;
-    } else if (prenda === "Jeans" && color === "Rojo" && talle === "L") {
-        return 1600;
-    } else if (prenda === "Jeans" && color === "Rojo" && talle === "M") {
-        return 1650;     
-    } else if (prenda === "Jeans" && color === "Rojo" && talle === "Xl") {
-        return 1700;
-    } else if (prenda === "Jeans" && color === "Verde" && talle === "Xs") {
-        return 1500;
-    } else if (prenda === "Jeans" && color === "Verde" && talle === "S") {
-        return 1550;
-    } else if (prenda === "Jeans" && color === "Verde" && talle === "L") {
-        return 1600;
-    } else if (prenda === "Jeans" && color === "Verde" && talle === "M") {
-        return 1650;     
-    } else if (prenda === "Jeans" && color === "Verde" && talle === "Xl") {
-        return 1700;
-    } else if (prenda === "Jeans" && color === "Amarillo" && talle === "Xs") {
-        return 1500;
-    } else if (prenda === "Jeans" && color === "Amarillo" && talle === "S") {
-        return 1550;
-    } else if (prenda === "Jeans" && color === "Amarillo" && talle === "L") {
-        return 1600;
-    } else if (prenda === "Jeans" && color === "Amarillo" && talle === "M") {
-        return 1650;     
-    } else if (prenda === "Jeans" && color === "Amarillo" && talle === "Xl") {
-        return 1700;
-    } else if (prenda === "Jeans" && color === "Azul" && talle === "Xs") {
-        return 1500;
-    } else if (prenda === "Jeans" && color === "Azul" && talle === "S") {
-        return 1550;
-    } else if (prenda === "Jeans" && color === "Azul" && talle === "L") {
-        return 1600;
-    } else if (prenda === "Jeans" && color === "Azul" && talle === "M") {
-        return 1650;     
-    } else if (prenda === "Jeans" && color === "Azul" && talle === "Xl") {
-        return 1700;
-    }
-
-    //vestidos
-
-     else if (prenda === "Vestido" && color === "Rojo" && talle === "Xs") {
-        return 3500;
-    } else if (prenda === "Vestido" && color === "Rojo" && talle === "S") {
-        return 3550;
-    } else if (prenda === "Vestido" && color === "Rojo" && talle === "L") {
-        return 3600;
-    } else if (prenda === "Vestido" && color === "Rojo" && talle === "M") {
-        return 3650;     
-    } else if (prenda === "Vestido" && color === "Rojo" && talle === "Xl") {
-        return 3700;
-    } else if (prenda === "Vestido" && color === "Verde" && talle === "Xs") {
-        return 3500;
-    } else if (prenda === "Vestido" && color === "Verde" && talle === "S") {
-        return 3550;
-    } else if (prenda === "Vestido" && color === "Verde" && talle === "L") {
-        return 3600;
-    } else if (prenda === "Vestido" && color === "Verde" && talle === "M") {
-        return 3650;     
-    } else if (prenda === "Vestido" && color === "Verde" && talle === "Xl") {
-        return 3700;
-    } else if (prenda === "Vestido" && color === "Amarillo" && talle === "Xs") {
-        return 3500;
-    } else if (prenda === "Vestido" && color === "Amarillo" && talle === "S") {
-        return 3550;
-    } else if (prenda === "Vestido" && color === "Amarillo" && talle === "L") {
-        return 3600;
-    } else if (prenda === "Vestido" && color === "Amarillo" && talle === "M") {
-        return 3650;     
-    } else if (prenda === "Vestido" && color === "Amarillo" && talle === "Xl") {
-        return 3700;
-    } else if (prenda === "Vestido" && color === "Azul" && talle === "Xs") {
-        return 3500;
-    } else if (prenda === "Vestido" && color === "Azul" && talle === "S") {
-        return 3550;
-    } else if (prenda === "Vestido" && color === "Azul" && talle === "L") {
-        return 3600;
-    } else if (prenda === "Vestido" && color === "Azul" && talle === "M") {
-        return 3650;     
-    } else if (prenda === "Vestido" && color === "Azul" && talle === "Xl") {
-        return 3700;
-
-    //Sacos
-
-    } else if (prenda === "Saco" && color === "Rojo" && talle === "Xs") {
-        return 4500;
-    } else if (prenda === "Saco" && color === "Rojo" && talle === "S") {
-        return 4550;
-    } else if (prenda === "Saco" && color === "Rojo" && talle === "L") {
-        return 4600;
-    } else if (prenda === "Saco" && color === "Rojo" && talle === "M") {
-        return 4650;     
-    } else if (prenda === "Saco" && color === "Rojo" && talle === "Xl") {
-        return 4700;
-    } else if (prenda === "Saco" && color === "Verde" && talle === "Xs") {
-        return 4500;
-    } else if (prenda === "Saco" && color === "Verde" && talle === "S") {
-        return 4550;
-    } else if (prenda === "Saco" && color === "Verde" && talle === "L") {
-        return 4600;
-    } else if (prenda === "Saco" && color === "Verde" && talle === "M") {
-        return 4650;     
-    } else if (prenda === "Saco" && color === "Verde" && talle === "Xl") {
-        return 4700;
-    } else if (prenda === "Saco" && color === "Amarillo" && talle === "Xs") {
-        return 4500;
-    } else if (prenda === "Saco" && color === "Amarillo" && talle === "S") {
-        return 4550;
-    } else if (prenda === "Saco" && color === "Amarillo" && talle === "L") {
-        return 4600;
-    } else if (prenda === "Saco" && color === "Amarillo" && talle === "M") {
-        return 4650;     
-    } else if (prenda === "Saco" && color === "Amarillo" && talle === "Xl") {
-        return 4700;
-    } else if (prenda === "Saco" && color === "Azul" && talle === "Xs") {
-        return 4500;
-    } else if (prenda === "Saco" && color === "Azul" && talle === "S") {
-        return 4550;
-    } else if (prenda === "Saco" && color === "Azul" && talle === "L") {
-        return 4600;
-    } else if (prenda === "Saco" && color === "Azul" && talle === "M") {
-        return 4650;     
-    } else if (prenda === "Saco" && color === "Azul" && talle === "Xl") {
-        return 4700;
-
-}
+function finalizarCompra () {
+    let totalPrecio = arrayProductos.reduce((acumulador, producto) => acumulador + producto, 0);
+    alert("El total de su compra es " + totalPrecio + " ETH")
+    alert("Gracias por elegir Mi-Tienda para adquirir NFT's =)")
 }
 
-function presupuesto (nombre, color, talle, precio){
-    alert("El precio de " + nombre + " de color " + color + " y de talle " + talle + "\nes de $" + precio)
+let opcion = menu();
+switch(opcion){
+    case 1:
+        carrito();
+        break;
+    case 2:
+        finalizarCompra();
+        break;
+    case 3:
+        salir();
+        break;
+    default:
+    alert("Opción incorrecta");
+    break;
 }
 
-function cobrar (nombre, precio){
-    let pago;
-    do {
-        pago = parseInt(prompt("Con cuanto pagas?. El precio del producto " + nombre + " es de $" + precio));
-        while (isNaN(pago)) {
-            alert("Ingrese un valor númerico");
-            pago = parseInt(prompt("Con cuanto pagas?. El precio del producto " + nombre + " es de $" + precio));
-        }
-    } while (pago < precio);
-    alert("Gracias!, " + nombreUsuario + ". Ha comprado el producto: " + nombre + ". Su vuelto es de $" + (pago - precio))
-}
-
-function saludoFinal (){
-    alert("Gracias por elegirnos, " + nombreUsuario);{
-    }
-}
-
-solicitarNombre();
-let prendaNombre = elegirPrenda();
-let colorNombre = elegirColor();
-let talleNombre = elegirTalle();
-let precioProducto = validarPrecio(prendaNombre, colorNombre, talleNombre);
-presupuesto(prendaNombre, colorNombre, talleNombre, precioProducto);
-cobrar(prendaNombre, precioProducto);
-saludoFinal();
