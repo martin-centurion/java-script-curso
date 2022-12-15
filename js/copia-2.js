ae368aa48730e26aad7df966d22a1a5b9612c056
martin-centurion <martinalejandrocenturion@gmail.com>
2022-12-14 22:46:14 -0300

    corrigiendo carrito

--- js/copia-2.js
+++ js/copia-2.js
@@ -1,11 +1,27 @@
 
+// class Cliente {
+//     constructor(nombre, direccion, telefono, mail){
+//         this.nombre = nombre;
+//         this.direccion = direccion;
+//         this.telefono = telefono;
+//         this.mail = mail;
+//     }
+// }
+
+// const clienteUno = new Cliente("Marcos", "Francia 4260, Florida Oeste, Provincia de Buenos Aires", 5491156355415, "marcos@gmail.com");
+// const clienteDos = new Cliente("Vanesa", "Leloir 549, Marcos Paz, Provincia de Buenos Aires", 5492525321, "vanesa@gmail.com");
+
+// const arrayClientes = [clienteUno, clienteDos];
+
+/* PRODUCTOS */
+
 class Producto {
     constructor(nombre, precio, id){
         this.nombre = nombre;
         this.precio = precio;
         this.id = id;
     }
-};
+}
 
 const buzoUno = new Producto("Buzo1", 12400, 1);
 const buzoDos = new Producto("Buzo2", 13000, 2);
@@ -20,12 +36,12 @@ function menu (){
     alert("Bienvenidos a Tienda de Ropa.");
     const opcion = parseInt(prompt("Ingrese una opción: \n1) Comprar.\n2)Finalizar Compra\n3)Salir."));
     return opcion;
-};
+}
 
  function seguir (){
      const compra = parseInt(prompt("Ingrese una opción: \n1)Seguir Comprando\n2)Finalizar Compra\n3) Salir"));
      return compra;
- };
+ }
 
 function carrito () {
     const usuarioCompra = parseInt(prompt("Escriba el id del producto que desea adquirir:\n\n1 - Buzo1 $12400\n2 - Buzo 13000 \n3 - Buzo 12000\n4 - Buzo $13000\n5 - Finalizar Compra"));
@@ -36,14 +52,14 @@ function carrito () {
     };
     arrayProductos.push(productoElegido.precio);
     carrito();
-};
+}
 
 
 function finalizarCompra () {
     let totalPrecio = arrayProductos.reduce((acumulador, producto) => acumulador + producto, 0);
     alert("El total de su compra es " + totalPrecio + " $");
     alert("Gracias por elegirnos");
-};
+}
 
 function salir() {
     alert("Gracias por visitar nuestra tienda");
@@ -65,3 +81,18 @@ switch(opcion){
     break;
 }
 
+// let compra = seguir();
+// switch (compra){
+//     case 1:
+//         carrito();
+//         break;
+//     case 2:
+//         finalizarCompra();
+//         break;
+//     case 3:
+//         salir();
+//         break;
+//     default:
+//     alert("Opción incorrecta");
+//     break;
+// }
